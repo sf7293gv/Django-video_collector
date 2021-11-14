6454b728da0d212bb6b6bfe37c4892bb10ca90de
woopty WOO <woopty@woo.com>
2021-11-14 17:25:52 -0600

    Added messages to show user when submissions occur

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import VideoForm
 
 def home(request):
@@ -6,5 +7,15 @@ def home(request):
     return render(request, 'video_collection/home.html', {'app_name': app_name})
 
 def add(request):
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'New Video Saved')
+            #todo show success message or redirect
+        else:
+            messages.warning(request, 'Enter valid data')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
