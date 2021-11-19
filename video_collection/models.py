d7c1ec8c106fe5841ab0c65883f3861f2ae7c38d
woopty WOO <woopty@woo.com>
2021-11-19 11:37:57 -0600

    Added a search term form and button

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,10 +1,33 @@
+from urllib import parse
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        # extract vid id from youtube url
+        if not self.url.startswith('https://www.youtube.com/watch'):
+            raise ValidationError(f'Not a YouTube url {self.url}')
+            
+        url_components = parse.urlparse(self.url)
+        query_string = url_components.query
+        if not query_string:
+            raise ValidationError(f'Invalid YouTube url {self.url}')
+        parameters = parse.parse_qs(query_string, strict_parsing=True)
+        v_parameters_list = parameters.get('v')
+        if not v_parameters_list:
+            raise ValidationError(f'Invalid YouTube url, missing parameters {self.url}')
+        self.video_id = v_parameters_list[0]
+
+        super().save(*args, **kwargs)
+            
 
     def __str__(self):
-        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Video ID: {self.video_id}, Notes: {self.notes[:200]}'
 
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import VideoForm
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from .forms import SearchForm, VideoForm
 from .models import Video
 
 def home(request):
@@ -11,17 +13,32 @@ def add(request):
     if request.method == 'POST':
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save()
-            messages.info(request, 'New Video Saved')
-            #todo show success message or redirect
-        else:
-            messages.warning(request, 'Enter valid data')
-            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                # messages.info(request, 'New Video Saved')
+                #todo show success message or redirect
+            except ValidationError:
+                messages.warning(request, 'Invalid Youtube URL')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video')
+        
+        messages.warning(request, 'Enter valid data')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
 
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
 
 
 def video_list(request):
+
+    search_form = SearchForm(request.GET)
+    if search_form.is_valid():
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(name__icontains=search_term).order_by('name')
+    else:
+        search_form = SearchForm()
+        videos = Video.objects.order_by('name')
+
     videos = Video.objects.all()
-    return render(request, 'video_collection/video_list.html', {'videos': videos})
+    return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form})
