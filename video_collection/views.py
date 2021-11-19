41e6232fdc6edb630a240f651c492f556119d405
woopty WOO <woopty@woo.com>
2021-11-19 12:18:10 -0600

    Added clear search button and num of vids

--- video_collection/views.py
+++ video_collection/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
-from .forms import SearchForm, VideoForm
+from django.db.models.functions import Lower
+from .forms import VideoForm, SearchForm
 from .models import Video
 
 def home(request):
@@ -35,10 +36,10 @@ def video_list(request):
     search_form = SearchForm(request.GET)
     if search_form.is_valid():
         search_term = search_form.cleaned_data['search_term']
-        videos = Video.objects.filter(name__icontains=search_term).order_by('name')
+        videos = Video.objects.filter(name__icontains=search_term).order_by(Lower('name'))
     else:
         search_form = SearchForm()
-        videos = Video.objects.order_by('name')
+        videos = Video.objects.order_by(Lower('name'))
 
     videos = Video.objects.all()
     return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form})
